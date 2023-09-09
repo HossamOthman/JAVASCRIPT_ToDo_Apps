ed4d5edfce4cf76410d08e2f61ed95e04f4a4a81
Hossam Othman <rowheadrex@gmail.com>
2023-09-10 00:52:14 +0200

    delete list

--- 2nd_todo/script.js
+++ 2nd_todo/script.js
@@ -1,10 +1,25 @@
 const listsContainer = document.querySelector('[data-lists]');
 const newListForm = document.querySelector('[data-new-list-form]');
 const newListInput = document.querySelector('[data-new-list-input]');
+const deleteListButton = document.querySelector('[data-delete-list-button]')
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListID = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
 
+listsContainer.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'li') {
+        selectedListID = e.target.dataset.listId;
+        saveAndRender();
+    }
+})
+
+deleteListButton.addEventListener('click', e => {
+    lists = lists.filter(list => list.id !== selectedListID);
+    selectedListID = null;
+    saveAndRender();
+})
 
 newListForm.addEventListener('submit', e => {
     e.preventDefault();
@@ -28,7 +43,8 @@ function saveAndRender() {
 }
 
 function save(){
-    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListID);
 }
 
 function render() {
@@ -38,6 +54,9 @@ function render() {
         listElement.dataset.listId = list.id;
         listElement.classList.add('list-name');
         listElement.innerText = list.name;
+        if (list.id === selectedListID) {
+            listElement.classList.add('active-list')
+        }
         listsContainer.appendChild(listElement);
     })
 };
