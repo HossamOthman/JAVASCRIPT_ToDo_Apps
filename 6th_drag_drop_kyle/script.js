9a2c2eb189123fd007b9ef7ef3198de5cf95daf0
Hossam Othman <rowheadrex@gmail.com>
2023-09-16 00:05:43 +0200

    set position is done

--- 6th_drag_drop_kyle/script.js
+++ 6th_drag_drop_kyle/script.js
@@ -15,7 +15,29 @@ draggables.forEach(elem => {
 containers.forEach(elem => {
     elem.addEventListener('dragover', e => {
         e.preventDefault();
+        const afterElement = getDragAfterElement(elem, e.clientY);
         const draggable = document.querySelector('.dragging');
-        elem.appendChild(draggable)
+        // console.log(afterElement)
+        if (afterElement == null) {
+            elem.appendChild(draggable)
+        } else {
+            elem.insertBefore(draggable, afterElement)
+        }
     });
-})
+});
+
+// function to determine the order of elements
+function getDragAfterElement(container, y) {
+   const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')];
+
+   return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect();
+    const offset = y - box.top - box.height / 2;
+    // console.log(offset)
+    if (offset < 0 && offset > closest.offset) {
+        return {offset: offset, element:child}
+    } else {
+        return closest;
+    }
+   }, {offset: Number.NEGATIVE_INFINITY}).element
+};
