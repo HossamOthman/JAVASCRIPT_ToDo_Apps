3995dd31ca5d55eced48d9906481bc2961b8e53c
Hossam Othman <rowheadrex@gmail.com>
2023-09-16 11:57:52 +0200

    drag drop finished

--- 6th_drag_drop_kyle/script.js
+++ 6th_drag_drop_kyle/script.js
@@ -45,3 +45,55 @@ function getDragAfterElement(container, y) {
 ////////////////////////////////////////////////////////
 // drag picture section
 
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty')
+
+// fill listeners
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+
+// drag functions
+function dragStart() {
+    this.className += ' hold';
+    setTimeout(() => this.className = 'invisible', 0 );
+};
+
+function dragEnd() {
+    this.className = 'fill'
+};
+
+
+// now handling the empties... looping through the empties
+
+// for (const empty of empties) {
+//     empty.addEventListener('dragover', dragOver);
+//     empty.addEventListener('dragenter', dragEnter);
+//     empty.addEventListener('dragLeave', dragLeave);
+//     empty.addEventListener('drop', dragDrop);
+// }
+empties.forEach(empty => {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+});
+
+function dragOver(e) {
+    e.preventDefault();
+    
+};
+
+function dragEnter(e) {
+    e.preventDefault();
+   this.className += ' hovered';
+};
+
+function dragLeave() {
+    this.className = 'empty'
+};
+
+function dragDrop() {
+    this.className = 'empty';
+    this.append(fill)
+};
